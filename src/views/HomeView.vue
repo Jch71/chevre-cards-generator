eadbe960182abc28bf3a8251c6bdab0261844730
Jean-Charles BONNARD <jc.bonnard@gyptis.fr>
2024-05-02 19:17:32 +0200

    :sparkle: ajout de formulaire

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,38 @@
 <template>
   <FormKit
   type="form"
-  id="registration-example"
+  id="registration-form"
   :form-class="submitted ? 'hide' : 'show'"
   submit-label="Register"
   @submit="submitHandler"
   :actions="false"
   #default="{ value }"
   >
-  <h1 class="text-2xl font-bold mb-2">Register!</h1>
+  <h1 class="text-2xl font-bold mb-2">S'enregistrer !</h1>
     <FormKit type="group"  v-model="address">
-    
+      <div class="double">
+        <FormKit
+        type="text"
+        label="Nom"
+        name="lastName"
+        placeholder="Nom"
+        validation="required"
+        />
+        <FormKit
+        type="text"
+        label="Prénom"
+        placeholder="Prénom"
+        name="firstName"
+        validation="required"
+      />
+      </div>
+      <FormKit
+      type="text"
+      label="Votre email"
+      name="email"
+      placeholder="Email"
+      validation="required"
+      />
       <FormKit
       type="text"
       label="Votre adresse"
@@ -32,6 +54,13 @@
       label="Votre code postal"
       placeholder="Code postal"
       validation="required|number"
+      />
+      <FormKit
+      type="text"
+      name="cardCode"
+      label="Votre code d'activation"
+      placeholder="Code d'activation"
+      validation="required|text"
       />
     </FormKit>
   <FormKit type="submit" label="Valider" />
@@ -55,3 +84,23 @@ async function submitHandler() {
 }
 
 </script>
+
+<style scoped>
+ #registration-form {
+  width: 320px;
+ }
+
+ .double {
+    display: flex;
+    justify-content: space-between;
+    div:first-child {
+      margin-right: 5px;
+    }
+    div:last-child {
+      margin-left: 5px;
+    }
+
+ }
+
+
+</style>
